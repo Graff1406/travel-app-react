ad5fe417bf7df9f6320c0602887f39243deacfad
avtandil <shainidze@gmail.com>
2021-12-22 20:19:16 +0200

    fires commit

---
+++ src/layouts/default/DefaultLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './default-layout.css'
+
+function DefaultLayout() {
+  return (
+    <div>
+      <h1>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default DefaultLayout
